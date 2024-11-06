93cc9a08ea5ee87197f87584839c3fffd49a2d0e
Guilherme Canuto <ganuto@hotmail.com>
2024-11-06 14:37:45 -0300

    ToString added to RuleCondition entity

--- src/main/java/com/project/manual/medicalquestionnaire/domain/RuleCondition.java
+++ src/main/java/com/project/manual/medicalquestionnaire/domain/RuleCondition.java
@@ -2,9 +2,11 @@ package com.project.manual.medicalquestionnaire.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class RuleCondition {
   private String questionId;
   private String choiceId;
