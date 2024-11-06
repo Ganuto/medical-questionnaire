9c14b9763fdf69d9a9ae83e72d037bd36bdb2063
Guilherme Canuto <ganuto@hotmail.com>
2024-11-06 17:11:23 -0300

    Initialized QuestionnaireIT

---
+++ src/integration-test/java/com/project/manual/medicalquestionnaire/QuestionnaireIT.java
@@ -0,0 +1,14 @@
+package com.project.manual.medicalquestionnaire;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class QuestionnaireIT {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    
+}
